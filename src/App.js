82f728df20722aa121c905e31b5501c21c618a18
Liam Ma <liam.q.ma@gmail.com>
2022-08-28 15:58:00 +1000

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { format, parse, differenceInCalendarDays } from "date-fns";
+import Cookies from "js-cookie";
+import "./App.css";
+import { renderIntoDocument } from "react-dom/test-utils";
+
+const dateFormate = "dd/MM/yyyy";
 
 function App() {
+  const [name, setName] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    try {
+      setData(JSON.parse(Cookies.get("data")));
+    } catch (e) {}
+  }, []);
+
+  const onAdd = () => {
+    const newData = [
+      ...data,
+      {
+        name,
+        startDate: format(new Date(), dateFormate),
+        datesDidIt: [],
+      },
+    ];
+    setData(newData);
+    Cookies.set("data", JSON.stringify(newData), { expires: 365 });
+  };
+
+  const didIt = (index) => {
+    const datesDidIt = data[index].datesDidIt;
+    const dateDidIt = format(new Date(), dateFormate);
+
+    if (datesDidIt.includes(dateDidIt)) return;
+
+    const newDate = [
+      ...data.filter((item, i) => i !== index),
+      {
+        ...data[index],
+        datesDidIt: [
+          ...data[index].datesDidIt,
+          format(new Date(), dateFormate),
+        ],
+      },
+    ];
+    setData(newDate);
+    Cookies.set("data", JSON.stringify(newDate), { expires: 365 });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        type="text"
+        placeholder="to don't"
+      />{" "}
+      <button onClick={onAdd}>Add</button>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>
+            {item.name}{" "}
+            {differenceInCalendarDays(
+              new Date(),
+              parse(item.startDate, dateFormate, new Date())
+            ) +
+              1 -
+              item.datesDidIt.length}{" "}
+            days <button onClick={() => didIt(index)}>:(</button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
