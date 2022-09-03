27d7f093ad569e2aac8a367a574c303842cb2298
Liam Ma <liam.q.ma@gmail.com>
2022-09-04 09:39:19 +1000

    add target and progress bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { useState, useEffect } from "react";
-import { format, parse, differenceInCalendarDays } from "date-fns";
+import {
+  format,
+  parse,
+  differenceInCalendarDays,
+  startOfTomorrow,
+} from "date-fns";
 import Cookies from "js-cookie";
 import "./App.css";
 
 const dateFormate = "dd/MM/yyyy";
+// a new habit usually takes a little more than 2 months — 66 days to be exact — and as much as 254 days until it’s fully formed
+// https://medium.com/swlh/21-day-habit-timeline-how-to-form-a-habit-in-21-days-day-by-day-92298446bf6b
+const target = 21;
 
 function App() {
   const [name, setName] = useState("");
@@ -21,7 +29,6 @@ function App() {
       {
         name,
         startDate: format(new Date(), dateFormate),
-        datesDidIt: [],
       },
     ];
     setData(newData);
@@ -29,19 +36,11 @@ function App() {
   };
 
   const didIt = (index) => {
-    const datesDidIt = data[index].datesDidIt;
-    const dateDidIt = format(new Date(), dateFormate);
-
-    if (datesDidIt.includes(dateDidIt)) return;
-
     const newDate = [
       ...data.filter((item, i) => i !== index),
       {
         ...data[index],
-        datesDidIt: [
-          ...data[index].datesDidIt,
-          format(new Date(), dateFormate),
-        ],
+        startDate: format(startOfTomorrow(), dateFormate),
       },
     ];
     setData(newDate);
@@ -60,14 +59,22 @@ function App() {
       <ul>
         {data.map((item, index) => (
           <li key={index}>
-            {item.name}{" "}
+            You have not done "{item.name}" for{" "}
             {differenceInCalendarDays(
               new Date(),
               parse(item.startDate, dateFormate, new Date())
-            ) +
-              1 -
-              item.datesDidIt.length}{" "}
-            days <button onClick={() => didIt(index)}>:(</button>
+            ) + 1}{" "}
+            consecutive day(s) <button onClick={() => didIt(index)}>:(</button>
+            <br />
+            <progress
+              max={target}
+              value={
+                differenceInCalendarDays(
+                  new Date(),
+                  parse(item.startDate, dateFormate, new Date())
+                ) + 1
+              }
+            />
           </li>
         ))}
       </ul>
